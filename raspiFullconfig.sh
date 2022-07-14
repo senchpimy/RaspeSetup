066a945463c1670e214b604c0a5a5c7d5ef3751d
senchi <senchpimy@gmail.com>
2022-07-14 09:47:17 -0500

    board wrong library fixed

--- raspiFullconfig.sh
+++ raspiFullconfig.sh
@@ -5,32 +5,30 @@ sudo pip3 install --upgrade setuptools
 echo #############python setuptools installed#############
 cd ~
 sudo raspi-config nonint do_i2c 0
-echo #############i2c enabled#############
+echo "#############i2c enabled#############"
 sudo raspi-config nonint do_spi 0
-echo #############spi enabled#############
+echo "#############spi enabled#############"
 sudo raspi-config nonint do_serial 0
-echo #############serial comunication enabled#############
+echo "#############serial comunication enabled#############"
 sudo raspi-config nonint do_ssh 0
-echo #############ssh enabled#############
+echo "#############ssh enabled#############"
 sudo raspi-config nonint do_camera 0
-echo #############camera enabled#############
+echo "#############camera enabled#############"
 sudo raspi-config nonint disable_raspi_config_at_boot 0
-echo #############raspi-init disabled at boot#############
+echo "#############raspi-init disabled at boot#############"
 sudo apt-get install -y i2c-tools libgpiod-dev
-echo #############i2c-tools and libgiod-dev installed############# 
+echo "#############i2c-tools and libgiod-dev installed#############"
 pip3 install --upgrade RPi.GPIO
-echo #############pip3 RPi.GPIO installed#############
+echo "#############pip3 RPi.GPIO installed#############"
 pip3 install --upgrade adafruit-blinka
-echo #############blinka installed#############
+echo "#############blinka installed#############"
 pip3 install adafruit-circuitpython-ssd1306
-echo #############adafruit installed#############
+echo "#############adafruit installed#############"
 pip3 install subprocess.run 
-echo #############subrocess installed#############
+echo "#############subrocess installed#############"
 pip3 install python-time
-echo #############time installed#############
-pip3 install board
-echo #############board intalled#############
+echo "#############time installed#############"
 sudo apt-get install -y python3-pil
-echo #############python-pil installed#############
+echo "#############python-pil installed#############"
 echo "|||||||||||||OLED CONFIG DONE|||||||||||||||||"
 
